1c87a5de3d10b615601a2ec02f9ab2b8ccd4acb3
546245234 <546245234@qq.com>
2018-05-13 19:41:54 +0800

    something
    ++

--- server/config.js
+++ server/config.js
@@ -5,5 +5,5 @@ module.exports={
 	DB_user:'root',
 	DB_pass:'',
 	DB_NAME:'elm',
-	PORT:8080
+	PORT:8090
 }
--- vue_html/src/components/index.vue
+++ vue_html/src/components/index.vue
@@ -154,7 +154,7 @@
      推荐商家
     </div>
     <section class="shoplist">
-      <ListItem v-for="data in arr" :key="data.restaurant_id" :restaurant="data"></ListItem>
+      <listItem v-for="data in arr" :key="data.restaurant_id" :restaurant="data"></listItem>
     </section>
     <footer class="index-wrapper_1Rsz2pX">
      <div class="index-footer_Gtduid_">
@@ -180,7 +180,7 @@
 </template>
 
 <script>
-import ListItem from '@/components/list-item'
+import listItem from '@/components/list-item'
 
 export default {
   name: 'index',
@@ -190,7 +190,16 @@ export default {
     }
   },
   components:{
-    ListItem
+    listItem
+  },
+  methods:{
+    async loadPage(page=0){
+      return (await this.axios.get(`restaurant/${page}/8`)).data;
+    }
+  },
+  async mounted(){
+    this.arr = await this.loadPage(0);
+    console.log(this.arr)
   }
 }
 </script>
